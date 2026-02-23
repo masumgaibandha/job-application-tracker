52ca7bb52556aaf525b1f5b9f6e9b21da6596219
masumgaibandha <masumgaibandha@gmail.com>
2026-02-23 09:10:46 +0600

    total function added

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const totalCount = document.getElementById('total-count');
+const interviewCount = document.getElementById('interview-count');
+const rejectedCount = document.getElementById('rejected-count');
 
+const allCards = document.getElementById('all-cards')
+
+
+totalCount.innerText = allCards.children.length
